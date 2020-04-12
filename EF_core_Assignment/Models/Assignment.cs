f0404f2c9fe65fbd4f0f8480eefc6d50161546c3
Viktor Lundsgaard Andersen <vik.lund.and@gmail.com>
2020-04-12 18:01:21 +0200

    Added models

---
+++ EF_core_Assignment/Models/Assignment.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EF_core_Assignment.Models
+{
+    public class Assignment
+    {
+        
+
+
+        // Do later 
+
+        //public override string ToString()
+        //{
+        //    return string.Format("Laptop({0}, {1}, {2}, {3}, {4}, {6}, {5})", LaptopId, Speed, Hd, Price, Price, Product, Screen);
+        //}
+
+    }
+}
+
---
+++ EF_core_Assignment/Models/Teacher.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EF_core_Assignment.Models
+{
+    public class Teacher
+    {
+        
+
+        // Do later 
+
+        //public override string ToString()
+        //{
+        //    return string.Format("Laptop({0}, {1}, {2}, {3}, {4}, {6}, {5})", LaptopId, Speed, Hd, Price, Price, Product, Screen);
+        //}
+
+    }
+}
+
