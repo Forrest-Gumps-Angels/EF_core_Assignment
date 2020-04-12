aa54c75e2176a5e2379740c3f7e84deac5fed8b0
Viktor Lundsgaard Andersen <vik.lund.and@gmail.com>
2020-04-12 17:56:33 +0200

    Added dbcontext and Model

---
+++ EF_core_Assignment/Data/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EF_core_Assignment.Data
+{
+    public class AppDbContext : DbContext
+    {
+
+        public DbSet<Pc> pcs { get; set; }
+        public DbSet<Laptop> laptops { get; set; }
+        public DbSet<Printer> printers { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+                => options.UseSqlite("Data Source=products.db");
+
+    }
+}
---
+++ EF_core_Assignment/Models/Exercise.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EF_core_Assignment.Models
+{
+    public class Exercise
+    {
+        public int ExerciseId { get; set; }
+        public string help_where { get; set; }
+        public string lecture { get; set; }
+
+        // Missing munber.. Number.. something.. See ef diagram
+
+        public int TeacherId { get; set; }
+        public Teacher Teacher { get; set; }
+
+        public int AuId { get; set; }
+        public Student Student { get; set; }
+
+
+        // Do later 
+
+        //public override string ToString()
+        //{
+        //    return string.Format("Laptop({0}, {1}, {2}, {3}, {4}, {6}, {5})", LaptopId, Speed, Hd, Price, Price, Product, Screen);
+        //}
+
+    }
+}
+
