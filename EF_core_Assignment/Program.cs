5887e06564fcc7779f9530436eb50c9a776d20f5
Viktor Lundsgaard Andersen <vik.lund.and@gmail.com>
2020-04-12 17:32:38 +0200

    Add project files.

---
+++ EF_core_Assignment/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EF_core_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
